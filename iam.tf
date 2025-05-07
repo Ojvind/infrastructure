bd264d920934ec64f2201e4c5de440958d474c49
Öjvind Otterbjörk <ojvind.otterbjork@icloud.com>
2025-05-07 16:33:15 +0200

    Add roles for Ops Agent

---
+++ iam.tf
@@ -0,0 +1,12 @@
+# Log Writer & MetricWriter required for Ops Agent
+resource "google_project_iam_member" "touchpoint_logWriter" {
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.lovlla.email}"
+  project = google_project.this.id
+}
+
+resource "google_project_iam_member" "touchpoint_metricWriter" {
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.lovlla.email}"
+  project = google_project.this.id
+}
